b4803ae4a1a096f8136bfed2a57d28f0f2b69b2e
JULIA TREFAS <juliatrefas@JULIAs-MacBook-Air.local>
2019-04-22 17:20:53 +0200

    edited database config

--- resources/views/mails/projectRequest.blade.php
+++ resources/views/mails/projectRequest.blade.php
@@ -3,7 +3,7 @@ Hello,
 
 You have received a new request regarding your project.
 
-@component('mail::button', ['url' => env('APP_URL'). '/#/project-requests/'])
+@component('mail::button', ['url' => env('APP_URL'). '/#/project-requests/' . $projectClientId])
     See request
 @endcomponent
 
